8e818bb7a8f0409e24a99e8f8e31b869228771b1
BrsaDev <161748604+BrsaDev@users.noreply.github.com>
2024-11-29 17:37:11 -0300

    Update message.js

--- services/zapfy/message.js
+++ services/zapfy/message.js
@@ -1,8 +1,8 @@
 const axios = require("axios")
 
 
-const instanceKey = "2fdee70d-79ef-4b08-b074-c32c2cd0d30f"
-const instanceToken = "baa0ce87-7507-4806-908e-57277c6e3017"
+const instanceKey = ""
+const instanceToken = ""
 const baseUrl = `https://api.zapfy.me/v1/instance/${instanceKey}/token/${instanceToken}`
 module.exports = {
     zapfySendMessage: async (destination, message) => {
